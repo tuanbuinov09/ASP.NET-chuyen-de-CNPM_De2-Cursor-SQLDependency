e3851d58e57368048e7ab473265829a3b0dc42f7
Tuan EnderMan <t01684599820@gmail.com>
2022-04-17 22:35:06 +0700

    Add project files.

---
+++ ChuyenDeCNPM_De2_Cursor_SQLDependency/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChuyenDeCNPM_De2_Cursor_SQLDependency.Startup))]
+namespace ChuyenDeCNPM_De2_Cursor_SQLDependency
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
